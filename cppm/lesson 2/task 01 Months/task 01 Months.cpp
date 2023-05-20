40f8995f4c14b12f16700c9f0c69301c71488351
Nesteruk Sergey <nesterukserser@gmail.com>
2023-05-21 00:02:42 +0300

    create lesson 2

---
+++ cppm/lesson 2/task 01 Months/task 01 Months.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+enum Months
+{
+
+};
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    int user_num = 0;
+
+    do {
+        std::cout << "Введите номер месяца: ";
+        std::cin >> user_num;
+    } while (user_num != 0);
+
+    std::cout << "До свидания";
+    
+
+
+    
+}
+
