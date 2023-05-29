e72b1eab876c6fa2e7ffd58e3e2e990d7f5443ab
Nesteruk Sergey <nesterukserser@gmail.com>
2023-05-29 21:18:00 +0300

    les03-task1 complete error after both inputs

--- cppm/lesson 3/task 1 Calc/task 1 Calc.cpp
+++ cppm/lesson 3/task 1 Calc/task 1 Calc.cpp
@@ -1,6 +1,86 @@
 ﻿#include <iostream>
 
+class Calculator
+{
+public:
+	double num1, num2;
+
+	double add()
+	{
+		return num1 + num2;
+	}
+	double multiply()
+	{
+		return num1 * num2;
+	}
+	double subtract_1_2()
+	{
+		return num1 - num2;
+	}
+	double subtract_2_1()
+	{
+		return num2 - num1;
+	}
+	double divide_1_2()
+	{
+		return num1 / num2;
+	}
+	double divide_2_1()
+	{
+		return num2 / num1;
+	}
+	bool set_num1(double num1)
+	{
+		if (num1 != 0)
+		{
+			this->num1 = num1;
+			return true;
+		}
+		else
+			return false;
+	}
+	bool set_num2(double num2)
+	{
+		if (num2 != 0)
+		{
+			this->num2 = num2;
+			return true;
+		}
+		else
+			return false;
+		
+	}
+
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "");
+
+	Calculator calc;
+	double number1 = 0, number2 = 0; 
+
+	do
+	{
+		std::cout << "Введите num1: ";
+		std::cin >> number1;
+		std::cout << "Введите num2: ";
+		std::cin >> number2;
+
+		if (calc.set_num1(number1) && calc.set_num2(number2))
+		{
+			std::cout << "num1 + num2 = " << calc.add()<<std::endl;
+			std::cout << "num1 - num2 = " << calc.subtract_1_2() << std::endl;
+			std::cout << "num2 - num1 = " << calc.subtract_2_1() << std::endl;
+			std::cout << "num1 * num2 = " << calc.multiply() << std::endl;
+			std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
+			std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
+		}
+		else
+		{
+			std::cout << "Неверный ввод!"<<std::endl;
+		}
+
+	} while (true);
+
 }
