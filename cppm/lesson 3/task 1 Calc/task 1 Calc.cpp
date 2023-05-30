59abcfd8d16bf730a3e9d4f1838afeeb89f3b7e5
Nesteruk Sergey <nesterukserser@gmail.com>
2023-05-30 21:17:20 +0300

    les03-task01 complete - error output after every incorrect input,still not started from the error

--- cppm/lesson 3/task 1 Calc/task 1 Calc.cpp
+++ cppm/lesson 3/task 1 Calc/task 1 Calc.cpp
@@ -64,22 +64,40 @@ int main()
 	{
 		std::cout << "Введите num1: ";
 		std::cin >> number1;
-		std::cout << "Введите num2: ";
-		std::cin >> number2;
-
-		if (calc.set_num1(number1) && calc.set_num2(number2))
+		if (calc.set_num1(number1))
 		{
-			std::cout << "num1 + num2 = " << calc.add()<<std::endl;
+			std::cout << "Введите num2: ";
+			std::cin >> number2;
+			if (calc.set_num2(number2))
+			{
+				std::cout << "num1 + num2 = " << calc.add() << std::endl;
+				std::cout << "num1 - num2 = " << calc.subtract_1_2() << std::endl;
+				std::cout << "num2 - num1 = " << calc.subtract_2_1() << std::endl;
+				std::cout << "num1 * num2 = " << calc.multiply() << std::endl;
+				std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
+				std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
+			}
+			else
+			{
+				std::cout << "Неверный ввод!" << std::endl;
+			}
+		}
+		else
+		{
+			std::cout << "Неверный ввод!" << std::endl;
+		}
+		
+
+		/*if (calc.set_num1(number1) && calc.set_num2(number2))
+		{
+			std::cout << "num1 + num2 = " << calc.add() << std::endl;
 			std::cout << "num1 - num2 = " << calc.subtract_1_2() << std::endl;
 			std::cout << "num2 - num1 = " << calc.subtract_2_1() << std::endl;
 			std::cout << "num1 * num2 = " << calc.multiply() << std::endl;
 			std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
 			std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
-		}
-		else
-		{
-			std::cout << "Неверный ввод!"<<std::endl;
-		}
+		}*/
+		
 
 	} while (true);
 
