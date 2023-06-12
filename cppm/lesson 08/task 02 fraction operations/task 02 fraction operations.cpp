2f8f19397a8417a252c2fb272a62561692bd3f3a
Nesteruk Sergey <nesterukserser@gmail.com>
2023-06-12 17:15:34 +0300

    create les08-task 02

---
+++ cppm/lesson 08/task 02 fraction operations/task 02 fraction operations.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+	void print()
+	{
+		std::cout << numerator_ <<"/"<< denominator_ << std::endl;
+	}
+
+	
+};
+
+int main()
+{
+	return 0;
+}
