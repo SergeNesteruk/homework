2186341a033749809690971670d1a25aa86bac68
Nesteruk Sergey <nesterukserser@gmail.com>
2023-07-08 08:07:42 +0300

    les04-task01 complete

--- cppm/lesson 04/task 01 Addresses/task 01 Addresses.cpp
+++ cppm/lesson 04/task 01 Addresses/task 01 Addresses.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
+#include <Windows.h>
 
 class address
 {
@@ -17,33 +18,55 @@ public:
         house_num = house;
         flat_num = flat;
     }
+
+    address ()
+    {    
+    };
+    void set_city(std::string new_city) { city = new_city; } //сеттеры для полей
+    void set_street(std::string new_street) { street = new_street; }
+    void set_house_num (int new_house) { house_num = new_house; }
+    void set_flat_num(int new_flat) { flat_num = new_flat; }
+
     void print()
     {
         std::cout << city << ", " << street << ", " << house_num << ", " << flat_num << std::endl; //для тестирования, как оно выглядит
     }
     std::string get_out_full_add()
     {
-        std::string x = city + ", " + street + ", " + std::to_string(house_num) + ", " + std::to_string(flat_num);
+        std::string x = city + ", " + street + ", " + std::to_string(house_num) + ", " + std::to_string(flat_num)+"\n";
         return x;
     }
  };
 
 int main()
 {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     std::ifstream fin("in.txt");
     std::ofstream fout("out.txt");
 
-    address test("Msc", "somestreet", 1, 1);
+    int size = 0;
+    fin >> size;
 
-    test.print();
+    address *arr_add = new address[size];
 
-    std::cout<<test.get_out_full_add();
+    std::string temp_city_in, temp_street_in;
+    int temp_house_in = 0, temp_flat_in = 0;
 
-    int add_num = 0;
-    fin >> add_num;    //введем количесво адресов
-    address* arr_add = new address[add_num];
-    
+    for (int i = 0; i < size; i++)
+    {
+        fin >> temp_city_in >> temp_street_in >> temp_house_in >> temp_flat_in;
+        arr_add[i].set_city(temp_city_in);
+        arr_add[i].set_street(temp_street_in);
+        arr_add[i].set_house_num(temp_house_in);
+        arr_add[i].set_flat_num(temp_flat_in);
+    }
 
+    fout << size<<"\n";
+    for (int i = size-1; i >= 0; i--)
+    {
+        fout << arr_add[i].get_out_full_add();
+    }
 
     fin.close();
     fout.close();
