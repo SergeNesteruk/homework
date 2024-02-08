eae100dba48cf4006e54af925cbe03428402d00c
Nesteruk Sergey <nesterukserser@gmail.com>
2024-02-09 01:34:05 +0300

    les01-task02 not finished, les02-task01 created, les03-task01 not finished

---
+++ cppl/lesson 01/task 02 std-variant/task 02 std-variant.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <variant>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+std::variant <int, std::string, std::vector<int>> get_variant() {
+    std::srand(std::time(nullptr));
+    int random_variable = std::rand() % 3;
+
+    std::variant<int, std::string, std::vector<int>> result;
+    switch (random_variable)
+    {
+    case 0:
+        result = 5;
+        break;
+    case 1:
+        result = "string";
+        break;
+    case 2:
+        result = std::vector<int>{ 1, 2, 3, 4, 5 };
+        break;
+    default:
+        break;
+    }
+    return result;
+}
+
+int main()
+{
+    std::cout<<
+    //int int_var;
+    //std::string string_var;
+    //std::vector vec_var{0};
+
+    //if (std::holds_alternative<int>(get_variant()) == true)
+    //{
+    //            
+    //}
+}
+
---
+++ cppl/lesson 03/task 01 smart array/task 01 smart array.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+class smart_array
+{
+private:
+	int size;
+	int memory_size;
+	int* ptr;
+public:
+	smart_array(int size)
+	{
+		this->size = size;
+		memory_size = size * 2;
+		int* ptr = new int[memory_size] {0};
+	}
+	~smart_array() {
+		delete[] ptr;
+	}
+	int* add_element(int val);
+	int get_element(int ind);
+};
+
+int smart_array::get_element(int ind)
+{
+	return ptr[ind];
+}
+int* smart_array::add_element(int val)
+{
+
+}
+
+int main()
+{
+
+/*	try {
+		smart_array arr(5);
+		arr.add_element(1);
+		arr.add_element(4);
+		arr.add_element(155);
+		arr.add_element(14);
+		arr.add_element(15);
+		std::cout << arr.get_element(1) << std::endl;
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}*/
+}
+
