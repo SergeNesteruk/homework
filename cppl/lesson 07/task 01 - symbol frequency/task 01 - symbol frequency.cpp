dae5a4328b7d1c31424574c68c4bdbc7cd9c98aa
Nesteruk Sergey <nesterukserser@gmail.com>
2024-04-06 14:21:58 +0300

    les07-task01 complete

--- cppl/lesson 07/task 01 - symbol frequency/task 01 - symbol frequency.cpp
+++ cppl/lesson 07/task 01 - symbol frequency/task 01 - symbol frequency.cpp
@@ -1,6 +1,32 @@
 ﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+
+	std::string str {"Hello world!!"};
+
+	std::cout << "[IN]: "<<str << "\n";
+
+	std::unordered_map<char, int> um;
+    for (const char c : str)
+        ++um[c];
+
+	std::vector <std::pair <char, int>> vec_map;
+	std::copy(um.begin(), um.end(), std::back_inserter(vec_map));
+
+	std::sort(
+		vec_map.begin(), vec_map.end(),
+		[](const auto& lhs, const auto& rhs) {
+			return lhs.second > rhs.second;
+		}
+	);
+
+	std::cout << "[OUT]: " << "\n";
+	for (auto elem : vec_map)
+	{
+		std::cout << elem.first << ": " << elem.second<< std::endl;
+	}
+	return 0;
 }
