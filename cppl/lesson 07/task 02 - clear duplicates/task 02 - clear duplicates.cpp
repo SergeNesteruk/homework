f443f7607ff91992664c30bf14d4b1acb167ed32
Nesteruk Sergey <nesterukserser@gmail.com>
2024-04-07 22:51:37 +0300

    les07-task02 create and complete

---
+++ cppl/lesson 07/task 02 - clear duplicates/task 02 - clear duplicates.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <functional>
+#include <list>
+
+int main()
+{
+    int num = 0;
+    std::cout << "[IN]:" << '\n';
+    std::cin >> num;
+
+    std::list<int> mylist{};
+    int elem = 0;
+
+    for (size_t i = 0; i < num; i++)
+    {
+        std::cin >> elem;
+        mylist.push_back(elem);
+    }
+
+    mylist.sort(std::greater<int>());
+    mylist.unique();
+
+    std::cout << "[OUT]:" << '\n';
+    for (const int i : mylist)
+    {
+        std::cout << i << '\n';
+    }
+}
