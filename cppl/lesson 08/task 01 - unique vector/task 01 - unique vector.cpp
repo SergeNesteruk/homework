08f47c6478e9551ebd711b9f27e3ddb4a2eacf0f
Nesteruk Sergey <nesterukserser@gmail.com>
2024-04-17 03:28:08 +0300

    les08-task01 & les08-task02 create and complete

---
+++ cppl/lesson 08/task 01 - unique vector/task 01 - unique vector.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void clearDoubles(std::vector<int>& vec)
+{
+    std::sort(vec.begin(), vec.end());
+    auto end = std::unique(vec.begin(), vec.end());
+    vec.erase(end, vec.end());
+}
+
+int main() {
+    std::vector<int> input_v{ 1, 1, 2, 5, 6, 1, 2, 4 };
+
+    std::cout << "[IN]: ";
+    for (auto elem : input_v)
+    {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+
+    clearDoubles(input_v);
+    std::cout << "[OUT]: ";
+    for (auto elem : input_v)
+    {
+        std::cout << elem << " ";
+    }
+}
---
+++ cppl/lesson 08/task 02 - print_container/task 02 - print_container.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <list>
+
+template <class T>
+void print_container(T& a)
+{
+    typename T::iterator it = a.begin();
+    while (it != a.end())
+    {
+        std::cout << *it << " ";
+        it++;
+    }
+    std::cout << std::endl;
+    // for (auto elem: a)
+    //   {
+    //     std::cout << elem << " ";
+    //   }
+    // std::cout<<std::endl;
+}
+
+int main() {
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+}
