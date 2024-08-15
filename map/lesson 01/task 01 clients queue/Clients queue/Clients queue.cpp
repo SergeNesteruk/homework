16e76e031769bc8d31dada61c421302eed3e5f4f
Nesteruk Sergey <nesterukserser@gmail.com>
2024-08-15 23:26:47 +0300

    map les01-task01 complete

--- map/lesson 01/task 01 clients queue/Clients queue/Clients queue.cpp
+++ map/lesson 01/task 01 clients queue/Clients queue/Clients queue.cpp
@@ -2,7 +2,56 @@
 #include <thread>
 #include <chrono>
 
+#include <Windows.h>
+
+class counter
+{
+private:
+    int count = 0;
+public:
+    int plus()
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        return count++;
+    }
+    int minus()
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        return count--;
+    }
+    void print()
+    {
+        std::cout << count << std::endl;
+    }
+
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    try
+    {
+        std::cout << "Введите максимальное число клиентов" << std::endl;
+        int maxclient;
+        std::cin >> maxclient;
+
+        counter c;
+        
+        for (int i = 0; i < maxclient; i++)
+        {
+            std::thread t1(&counter::plus, &c);
+            std::thread t2(&counter::minus, &c);
+            std::cout << "count = ";
+            t1.join();
+            t2.join();
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << "Exception happend: " << e.what() << std::endl;
+    }
+
+    
+    return 0; 
 }
